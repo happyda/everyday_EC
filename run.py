e9d3c0e9114b8c82c9a75771913eeeb9faeed55b
SodiumFoo <ul2you@gmail.com>
2020-07-23 10:14:56 +0000

    最後一版，about頁面問題不處理了

--- run.py
+++ run.py
@@ -19,6 +19,23 @@ def cart():
 def shop_single():
     return render_template('shop-single.html')
 
+@app.route("/check-out")
+def check_out():
+    return render_template('checkout.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+
+@app.route("/thankyou")
+def thankyou():
+    return render_template('thankyou.html')
+
 if __name__ == '__main__':
 
     app.run(debug=True, host='0.0.0.0')
